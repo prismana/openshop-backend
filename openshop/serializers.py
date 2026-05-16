bc66d14505e678ff56f51345d4ae745b8fe1a7b3
Rahdriawan Prismana <63779468+prismana@users.noreply.github.com>
2026-05-16 16:50:43 +0700

    Adding Get by pk. Now can update product item. Now can delete product item

--- openshop/serializers.py
+++ openshop/serializers.py
@@ -1,7 +1,38 @@
 from openshop.models import Product
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    _links = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ["id", "name", "description", "shop", "location", "price", "discount", "category", "stock", "is_available", "picture"]
+        fields = ["id", "name", "description", "shop", "location", "price", "discount", "category", "stock", "is_available", "picture", "_links"]
+
+    def get__links(self, obj):
+        request = self.context.get('request')
+        return [
+            {
+                "rel": "self",
+                "href": reverse('product-list', request=request),
+                "action": "POST",
+                "types": ["application/json"]
+
+            },
+            {
+                "rel": "self",
+                "href": reverse('product-detail', kwargs={'pk': obj.id}, request=request),
+                "action": "GET",
+                "types": ["application/json"]
+            },
+            {
+                "rel": "self",
+                "href": reverse('product-detail', kwargs={'pk': obj.id}, request=request),
+                "action": "PUT",
+                "types": ["application/json"]
+            },
+            {
+                "rel": "self",
+
+            }
+        ]
--- openshop/views.py
+++ openshop/views.py
@@ -1,8 +1,11 @@
+from django.core.serializers import serialize
+from django.http import Http404
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from openshop.serializers import ProductSerializer
+from .models import Product
 
 # Create your views here.
 class Productlist(APIView):
@@ -15,4 +18,35 @@ class Productlist(APIView):
         return Response(products.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
-        return Response('hello', status=status.HTTP_200_OK)
+        product = Product.objects.all()
+        serializer = ProductSerializer(product, many=True)
+        return Response({
+            "products": serializer.data,
+        }, status=status.HTTP_200_OK)
+
+class ProductDetail(APIView):
+    def get_product_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        product = self.get_product_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+
+    def put(self, request, pk):
+        product = self.get_product_object(pk)
+        serializer = ProductSerializer(product, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        product = self.get_product_object(pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
